fa8239702b6eb3070ee525842f6d7ed8e4b89b5f
STROKE-MODS <strokemods@gmail.com>
2026-08-01 22:27:04 +0530

    Third_part

---
+++ Week_1/Simple_interest/Simple_interest/Main.java
@@ -0,0 +1,8 @@
+package Simple_interest.Simple_interest;
+import java.util.Scanner;
+public class Main {
+    public static void main(String a[]){
+        
+        System.out.print("Enter the Simple interest : ");
+    }
+}
