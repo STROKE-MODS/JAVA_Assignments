9144a905bbd70c8c0572d07d29c61c3314275557
STROKE-MODS <strokemods@gmail.com>
2026-08-02 02:47:38 +0530

    4th_part

---
+++ Week_1/Simple_interest/Simple_interest/Forumlas.java
@@ -0,0 +1,10 @@
+package Simple_interest.Simple_interest;
+public class Forumlas{
+    double simle(double principle,double rate,double time){
+        return (principle*rate*time)/100;
+    }
+    double finalamount(double principle,double simpleint){
+        return principle+simpleint;
+    }
+    
+}
