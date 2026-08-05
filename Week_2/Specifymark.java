b86b1e9aaf0d49616709d38443176280d8ae6a94
STROKE-MODS <strokemods@gmail.com>
2026-08-05 23:30:50 +0530

    Week 2

---
+++ Week_2/Specifymark.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class GradingSystem{
+    String marks_return(int t){
+        if (t > 80 && t < 100) {
+         return "Distinction category";
+      } else if (t > 70 && t < 79) {
+         return "Merit Category";
+      } else {
+         return t > 50 && t < 69 ? "Pass category" : "Fail category";
+      }
+    }
+}
+public class Specifymark{
+    public static void main(String a[]){
+        Scanner scan = new Scanner(System.in);
+        GradingSystem gs  = new GradingSystem();
+        System.out.print("Enter your marks : ");
+        int t = scan.nextInt();
+
+        if(t<0){
+            System.out.println("Marks cannot be negative");
+        }
+        else if(t>100){
+            System.out.println("Marks cannot be greater then 100");
+        }
+        else{
+            System.out.println("You have scored : "+gs.marks_return(t));
+        }
+        scan.close();
+    }
+}
