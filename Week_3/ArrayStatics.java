3919406b6ee1bb02080d65be81fc54b279d94069
STROKE-MODS <hs619689@gmail.com>
2026-08-10 22:24:41 +0530

    Add files via upload

---
+++ Week_3/ArrayStatics.java
@@ -0,0 +1,8 @@
+public class ArrayStatics{
+    static int sum(int[] values);
+    static double average(int[] values);
+    static int minimum(int[] values)
+    static int maximum(int[] values)
+    static int countEven(int[] values)
+    static void printSignCounts(int[] values)
+}
