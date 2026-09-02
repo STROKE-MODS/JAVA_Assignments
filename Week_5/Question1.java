fabde75bc8833d3ac6225835773a5fecba7ce470
STROKE-MODS <hs619689@gmail.com>
2026-09-02 23:27:37 +0530

    Update Question1.java

--- Week_5/Question1.java
+++ Week_5/Question1.java
@@ -1,4 +1,10 @@
+import Java.util.Scanner;
 public static void main(String args[])
   {
-    System.out.println("Soon");
+    Scanner scan = new Scanner(System.in);
+    int a;
+    System.out.println("Enter the number : ");
+    a = scan.nextInt();
+    System.out.println("The number is : "+a);
+  return 0;
 }
