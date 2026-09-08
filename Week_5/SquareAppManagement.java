0e47251687f731c4e8ef5cdab2b15c6189c1bded
STROKE-MODS <hs619689@gmail.com>
2026-09-08 23:32:35 +0530

    Update SquareAppManagement.java

--- Week_5/SquareAppManagement.java
+++ Week_5/SquareAppManagement.java
@@ -194,6 +194,7 @@ public class SquareAppManagement {
         System.out.println("The maximum area is : "+max_Area);
         System.out.println("The largest shape is : x"+largestShape.getClass().getSimpleName());
         System.out.println("The Question is finished");
+        System.out.println("The Question is finished");
     }
 }
 
